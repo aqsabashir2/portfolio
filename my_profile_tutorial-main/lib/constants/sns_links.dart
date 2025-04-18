f83ed77c2e58d2658a926ec2ec8bf045c4884fb7
Shani Computers <shani@example.com>
2025-04-18 02:16:32 -0700

    Updated social links in sns_links.dart

--- my_profile_tutorial-main/lib/constants/sns_links.dart
+++ my_profile_tutorial-main/lib/constants/sns_links.dart
@@ -2,5 +2,4 @@ class SnsLinks {
   static const String github = "https://github.com/aqsabashir2";
   static const String linkedIn = "https://www.linkedin.com/in/aqsabashir/";
   static const String facebook = "https://www.facebook.com/aqsa.bashir.94043";
-  static const String About = "https://www.shohruhak.com/tech";
 }
